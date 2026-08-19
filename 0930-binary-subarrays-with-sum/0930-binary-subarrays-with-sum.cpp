f6dfa7b49fe006671b596d3d4b80f7632541a701
mang <tarunlapacc@gmail.com>
2026-08-19 18:14:31 +0530

    Time: 0 ms (100%), Space: 32.6 MB (83.29%) - LeetHub

--- 0930-binary-subarrays-with-sum/0930-binary-subarrays-with-sum.cpp
+++ 0930-binary-subarrays-with-sum/0930-binary-subarrays-with-sum.cpp
@@ -1,26 +1,22 @@
 class Solution {
 private:
-    int slidingWindowAtMost(vector<int>& nums,int goal){
+    int numSubarraysWithAtMostSum(vector<int>& nums, int goal){
         if(goal<0)return 0;
-
-        int left=0;
-        int currentSum=0;
-        int count=0;
-
-        for(int right=0;right<nums.size();right++){
-            currentSum+=nums[right];
-
-            while(currentSum>goal){
-                currentSum-=nums[left];
-                left++;
+        int left=0,right=0,sum=0,count=0;
+        while(right<nums.size()){
+            sum+=nums[right];
+            while(sum>goal){
+                sum=sum-nums[left];
+                left++;   
             }
             count+=(right-left+1);
+            right++;
         }
         return count;
     }
 public:
     int numSubarraysWithSum(vector<int>& nums, int goal) {
-        
-        return slidingWindowAtMost(nums,goal)-slidingWindowAtMost(nums,goal-1);
+        return numSubarraysWithAtMostSum(nums,goal)-numSubarraysWithAtMostSum(nums,goal-1);
     }
+   
 };
